85cd99cae36e7303933b1758352380a911809d29
liemchau <liemchau@kms-technology.com>
2017-07-07 08:13:40 +0700

    Commit scripts

---
+++ ConsoleApp1/FirstTestCase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace ConsoleApp1
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {            
+            IWebDriver driver = new ChromeDriver(@"D:\Drivers");
+            driver.Url = "http://www.demoqa.com";
+        }
+    }
+}
